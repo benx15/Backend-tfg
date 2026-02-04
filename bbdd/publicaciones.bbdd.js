ca81580943ad4f41bf82a62497ced3a69e2ec721
Beñat <zabalaortizbezo@gmail.com>
2026-02-04 11:42:54 +0100

    Publicaciones hechas

---
+++ bbdd/publicaciones.bbdd.js
@@ -0,0 +1,35 @@
+const publicaiones =
+[
+    {
+      "titulo": "Mi primer post en la plataforma",
+      "usuario": "698319c01844b897065a65da",
+      "contenido": "Hola a todos, este es mi primer post y estoy muy contenta de formar parte de la comunidad.",
+      "fecha": "2024-12-01T10:30:00.000Z"
+    },
+    {
+      "titulo": "Concierto increíble anoche",
+      "usuario": "698319c01844b897065a65dc",
+      "contenido": "Ayer fui a un concierto espectacular, la organización y el ambiente fueron brutales.",
+      "fecha": "2024-12-03T19:45:00.000Z"
+    },
+    {
+      "titulo": "Recomendaciones musicales",
+      "usuario": "698319c01844b897065a65de",
+      "contenido": "Os recomiendo escuchar nuevos artistas independientes, hay auténticas joyas.",
+      "fecha": "2024-12-05T12:15:00.000Z"
+    },
+    {
+      "titulo": "Evento imperdible este mes",
+      "usuario": "69831a281844b897065a65e8",
+      "contenido": "Este mes hay varios eventos interesantes, no os perdáis el del día 20.",
+      "fecha": "2024-12-08T09:00:00.000Z"
+    },
+    {
+      "titulo": "Mi experiencia usando la app",
+      "usuario": "698319c01844b897065a65dc",
+      "contenido": "La aplicación es muy intuitiva y fácil de usar, enhorabuena al equipo.",
+      "fecha": "2024-12-10T18:20:00.000Z"
+    }
+]
+
+module.exports=publicaiones
