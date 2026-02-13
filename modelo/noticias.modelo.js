62956877bf0844f87cd4bb7f155a0f2c32145cf1
beñat <zabalaortizbezo@gmail.com>
2026-02-13 10:11:58 +0100

    Cambios noticias

--- modelo/noticias.modelo.js
+++ modelo/noticias.modelo.js
@@ -5,15 +5,17 @@ const noticiaSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    tematica:{
+        type: String,
+        required:true,
+        trim: true,
+        enum: ["Grupos","Quedadas","Album o EP" , "Otros"],
+    },
     contenido:{
         type: String,
         required: true
     },
-    tema:{
-        type: String,
-        required: true
-    }
-
+    
 },{
     versionKey:false,
     timestamps:true
