1d67806e5f28b7e7eb3c8272a90d2ad63a72218c
Beñat <zabalaortizbezo@gmail.com>
2026-04-24 14:30:49 +0200

    ultima variable

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const rutaArtistas = require("./rutas/artistas.ruta")
 
 //Middleware
 app.use(cors({
-    origin: "*", 
+    origin: process.env.FRONTEND_URL || "http://localhost:4200",
     methods: ["GET","POST","PUT","DELETE"],
     credentials: true
 }));
